7cd8b542985586e8388c2c45023786d32845246d
Camille Caschili <camille.caschili@hoomano.com>
2023-06-27 18:02:06 +0200

    multiple changes of archi and nav

--- scripts/popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var button = document.getElementById("clickme");
-  button.addEventListener("click", function () {
-    alert("Vous avez cliqué sur le bouton !");
-  });
-});
