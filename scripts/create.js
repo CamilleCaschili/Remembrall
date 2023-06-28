9068d7c749f9d7346ac086dc43e057ffa7648293
Camille Caschili <camille.caschili@hoomano.com>
2023-06-28 14:47:14 +0200

    add items in local storage and print them in the index, simple list with just a text

--- scripts/create.js
+++ scripts/create.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-
 var backButton = document.getElementById("backButton");
+var addReminderButton = document.getElementById("addReminderButton");
+var reminderText = document.getElementById("reminderText");
+var reminderArray = [];
+
   backButton.addEventListener("click", function () {
     window.location.href = "index.html";
+  });
+
+  addReminderButton.addEventListener("click", function () {
+    reminderArray.push(reminderText.value);
+    reminderText.value = "";
+    localStorage.setItem("reminderArray", JSON.stringify(reminderArray));
+    alert("Reminder added!");
   });
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let reminders = []
-
-  var clearButton = document.getElementById("clearButton");
-  clearButton.addEventListener("click", function () {
-    localStorage.clear();
-    reminders = [];
-  });
-  
-  var addButton = document.getElementById("addButton");
-  addButton.addEventListener("click", function () {
-    reminders.push("test2"); // Push a new item into the array
-    localStorage.setItem("reminder", JSON.stringify(reminders)); // Store the last item in localStorage
-  });
-
-  var getButton = document.getElementById("getButton");
-  getButton.addEventListener("click", function () {
-    var data = JSON.parse(localStorage.getItem("reminder")); // Get the last item from localStorage
-    alert(data);
-  });
-
   var createButton = document.getElementById("createButton");
+  var reminderList = document.getElementById("reminderList");
+  var reminders = JSON.parse(localStorage.getItem("reminderArray")) || [];
+
+  for (var i = 0; i < reminders.length; i++) {
+    var reminder = reminders[i];
+    var li = document.createElement("li");
+    li.textContent = reminder;
+    reminderList.appendChild(li);
+  }
+
   createButton.addEventListener("click", function () {
     window.location.href = "../src/create.html";
   });
