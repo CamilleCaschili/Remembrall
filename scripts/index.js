ea126fd77c207c265bdf64f040e7fa9806078c0e
Camille Caschili <camille.caschili@hoomano.com>
2023-06-28 15:03:15 +0200

    add check

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
   var reminderList = document.getElementById("reminderList");
   var reminders = JSON.parse(localStorage.getItem("reminderArray")) || [];
 
-  for (var i = 0; i < reminders.length; i++) {
-    var reminder = reminders[i];
-    var li = document.createElement("li");
-    li.textContent = reminder;
-    reminderList.appendChild(li);
+  if (reminders) {
+    for (var i = 0; i < reminders.length; i++) {
+      var reminder = reminders[i];
+      var li = document.createElement("li");
+      li.textContent = reminder;
+      reminderList.appendChild(li);
+    }
   }
 
   createButton.addEventListener("click", function () {
